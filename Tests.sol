24c81b43c085eb16ba435431d19a8684a97a7f74
timeandmoneyceo <97771603+timeandmoneyceo@users.noreply.github.com>
2023-11-15 13:55:55 -0800

    Create Tests.sol

---
+++ Tests.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Test.sol
+contract Test {
+    function property_transfer(address _token, address _to, uint _amt) public {
+        require(_to != address(this));
+
+        TokenCorrect t = TokenCorrect(_token);
+
+        uint xPre = t.balanceOf(address(this));
+        require(xPre >= _amt);
+        uint yPre = t.balanceOf(_to);
+
+        t.transfer(_to, _amt);
+        uint xPost = t.balanceOf(address(this));
+        uint yPost = t.balanceOf(_to);
+
+        assert(xPost == xPre - _amt);
+        assert(yPost == yPre + _amt);
+    }
+}
